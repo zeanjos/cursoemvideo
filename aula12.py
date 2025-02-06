8f8cdfc2d4da72cdacba26a7c9322b1441d162ad
zeanjos <pegananjos@gmail.com>
2025-02-05 23:53:04 -0300

    Nomes

--- aula12.py
+++ aula12.py
@@ -2,9 +2,9 @@ nome = str(input('Qual é o seu nome: ')).strip().lower().capitalize()
 
 if nome == 'José':
     print('Que nome bonito!! Tenha um bom dia, {}'.format(nome))
-elif nome == 'José Gabriel':
-    print('O seu nome é o melhor do mundo!! Bom dia {}!!'.format(nome))
 elif nome == 'Pedro' or nome == 'Maria' or nome == 'João':
     print('O seu nome é bem comum no Brasil, {}'.format(nome))
+elif nome in 'Ana Cláudia Jéssica Juliana':
+     print('Que nome masculino esbelto! {}'.format(nome))
 else:
         print('Tenha um bom dia, {}'.format(nome))
