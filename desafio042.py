24a1eabfe113b09cd681da28c42e9d1edda88f35
zeanjos <pegananjos@gmail.com>
2025-02-11 22:47:08 -0300

    Equilatero

---
+++ desafio042.py
@@ -0,0 +1,7 @@
+r1 = int(input('Digite o valor do primeiro segmento: '))
+r2 = int(input('Digite o valor do segundo segmento: '))
+r3 = int(input('Digite o valor do terceiro segmento: '))
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2 and r1 == r2 and r1 == r3 and r2 == r3:
+    print('Todos os lados são iguais, um triângulo equilatero!')
+else:
+    print('Peido')
