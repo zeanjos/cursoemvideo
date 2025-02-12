a8f278935daa6a894585480f05b9b8b91ba7e5c5
zeanjos <pegananjos@gmail.com>
2025-02-11 23:01:24 -0300

    triangulos

--- desafio042.py
+++ desafio042.py
@@ -2,6 +2,10 @@ r1 = int(input('Digite o valor do primeiro segmento: '))
 r2 = int(input('Digite o valor do segundo segmento: '))
 r3 = int(input('Digite o valor do terceiro segmento: '))
 if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2 and r1 == r2 and r1 == r3 and r2 == r3:
-    print('Todos os lados são iguais, um triângulo equilatero!')
+    print('Todos os lados são iguais, um triângulo equilátero!')
+elif r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2 and (r1 == r2 or r1 == r3 or r2 ==3):
+    print('Este triângulo é isosceles, possui dois lados iguais!')
+elif r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2 and (r1 != r2 and r1 != r3 and r2 != r3):
+    print('Este triângulo é escaleno, possui todos os lados diferentes! ')
 else:
-    print('Peido')
+    print('Não é um triângulo!')
