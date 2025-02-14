0fb6887b584221750fd72032c8c3e303d8002d86
zeanjos <pegananjos@gmail.com>
2025-02-13 23:04:30 -0300

    Binario correto

--- Desafio036.py
+++ Desafio036.py
@@ -5,6 +5,6 @@ ano = anos*12
 passou = sal * 0.30
 total = valcasa / ano
 if total <= passou:
-    print('Você poderá prosseguir com o emprestimo pois o seu salário R${} Fica acima do esperado para o emprestimo.'.format(sal))
+    print('Você poderá prosseguir com o emprestimo pois o seu salário R${:.2f}\n Fica acima do esperado para o emprestimo, a parcela fica de: R${:.2f}'.format(sal, total))
 else:
-    print('Você não poderá prosseguir com o emprestimo pois o seu salário R${} Fica abaixo do esperado para o emprestimo.'.format(sal))
+    print('Você não poderá prosseguir com o emprestimo pois o seu salário R${:.2f}\n Fica abaixo do esperado para o emprestimo, a parcela fica de R${:.2f}'.format(sal, total))
--- desafio037.py
+++ desafio037.py
@@ -1,8 +1,9 @@
 numero = int( input ('Digite o número que deseja converter: '))
-escolha = int ( input ('Obrigado pela informação, agora escolha:\n 1 Para binário.\n 2 Para octal. \n 3 Para hexadecimal.'))
-binario = numero % 2
+escolha = int ( input ('Obrigado pela informação, agora escolha:\n 1- Para binário.\n 2- Para octal. \n 3- Para hexadecimal.\n Digite: '))
+
 if escolha == 1:
-    while numero > 0:
-        binario = ''
-        print(binario)
-        numero = numero //2
+    print('O número {} Convertido para binário é: {}'.format(numero, bin(numero)))
+elif escolha == 2:
+    print('O número{} Convertido para octal é: {}'.format(numero, oct(numero)))
+else:
+    print('O número {} Convertido para hexadecimal é {}'.format(numero, hex(numero)))
