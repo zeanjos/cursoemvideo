4ef0824b48880e22021bbcf14250141b019aebe1
zeanjos <pegananjos@gmail.com>
2025-02-13 22:26:44 -0300

    Jokenpo corrigido e jogavel

--- desafio045.py
+++ desafio045.py
@@ -7,6 +7,26 @@ if jok == 1:
     if aleatorio == 1:
         print('Você escolheu pedra, eu também escolhi pedra, empate!')
 elif jok ==2:
-    if aleatorio
-
-
+    if aleatorio == 1:
+        print('Eu escolhi papel e você pedra, eu ganhei!')
+elif jok == 2:
+    if jok == 2:
+        print('Eu escolhi papel e você também, empate!')
+elif jok == 1:
+    if aleatorio == 2:
+        print('Eu escolhi pedra e você papel, você ganhou!')
+elif jok == 2:
+    if aleatorio == 3:
+        print('Eu escolhi papel e você tesoura, você ganhou!')
+elif jok == 3:
+    if aleatorio == 2:
+        print('Eu escolhi tesoura e você papel, eu ganhei')
+elif jok == 3:
+    if aleatorio == 3:
+        print('Eu escolhi tesoura e você também, empate!')
+elif jok == 1:
+    if aleatorio == 3:
+        print('Eu escolhi pedra e você tesoura, eu ganhei!')
+elif jok == 3:
+    if aleatorio == 1:
+        print('Eu escolhi tesoura e você pedra, você ganhou!')
