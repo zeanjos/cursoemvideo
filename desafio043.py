09ec56c13eb5a669ade5068373584f089d2f8563
zeanjos <pegananjos@gmail.com>
2025-02-14 23:04:10 -0300

    boas práticas e correção básica de imc

--- desafio043.py
+++ desafio043.py
@@ -3,11 +3,12 @@ altura = float(input('Digite a sua altura em centimetros ')) / 100
 imc = peso / (altura**2)
 if imc <= 18.5:
     print('O seu IMC é de: {:.2f} Você está abaixo do peso'.format(imc))
-elif 18.5 < imc < 25:
+elif 18.5 <= imc < 25:
     print('O seu IMC é de: {:.2f} Você está no peso ideal'.format(imc))
-elif 25 < imc < 30:
+elif 25 <= imc < 30:
     print('O seu IMC é de: {:.2f} Você está com sobrepeso'.format(imc))
-elif 30 < imc < 40:
+elif 30 <= imc < 40:
     print('Tá gigantesco irmão, emagrece essa banha! Imc: {}'.format(imc))
 else:
     print('Vai morrer daqui 3 meses parcero!')
+    
