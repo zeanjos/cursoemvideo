377dff93e8520d12e93d226a3611dc960e6bd1fd
zeanjos <pegananjos@gmail.com>
2025-02-16 20:18:12 -0300

    regressão condicional for

--- aula013.py
+++ aula013.py
@@ -1,2 +1,7 @@
-for c in range(1,100):
-    print('Olá mundo!')
+
+for c in range(1,101):
+    print(c)
+print('Fim, foram 100 reps até a falha')
+for d in range (101, 0, -1):
+    print(d)
+print('Só regresso do 100 ao 1')
