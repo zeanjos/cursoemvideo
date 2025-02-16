85cff6e702c879b775934b79f6399afb8dbb564e
zeanjos <pegananjos@gmail.com>
2025-02-16 20:25:44 -0300

    Entre aspas um jogo

--- aula013.py
+++ aula013.py
@@ -1,7 +1,7 @@
 
-for c in range(1,101):
+for c in range(1,101, 2):
     print(c)
-print('Fim, foram 100 reps até a falha')
+print('\nFim, foram 100 reps até a falha pulando de dois em dois\n')
 for d in range (101, 0, -1):
     print(d)
 print('Só regresso do 100 ao 1')
