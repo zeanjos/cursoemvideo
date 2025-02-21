e687ee56f38f92c7a16af5bc365692175cc52807
zeanjos <pegananjos@gmail.com>
2025-02-20 22:58:50 -0300

    Adicionado a média de pesos

--- ex55.py
+++ ex55.py
@@ -1,7 +1,9 @@
 maior = 0
 menor = 0
+soma = 0
 for p in range(1, 6):
     peso = float(input('Digite o peso da {}ª Pessoa: '.format(p)))
+    soma += peso
     if p == 1:
         maior = peso
         menor = peso
@@ -10,4 +12,6 @@ for p in range(1, 6):
             maior = peso
         if peso < menor:
             menor = peso
+media = soma / p
 print('O maior peso é: {}\n O menor peso é: {}'.format(maior, menor))
+print('A média de pesos digitados é: {}'.format(media))
