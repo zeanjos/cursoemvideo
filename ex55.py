44bab9c529f7d033c73779b6673d481e6ec4c42e
zeanjos <pegananjos@gmail.com>
2025-02-20 22:44:30 -0300

    foi retirado os erros de burro!

--- ex55.py
+++ ex55.py
@@ -1,12 +1,13 @@
 maior = 0
 menor = 0
 for p in range (1,6):
-    peso = float(input('Digite o peso da {}ª Pessoa'))
+    peso = float(input('Digite o peso da {}ª Pessoa: '.format(p)))
     if p == 1:
-        maior == p
-        menor == p 
-else:
-    peso > maior:
-    maior = peso
-    peso < menor:
-    peso = menor
+        maior = peso
+        menor = peso 
+    else:
+        if peso > maior:
+            maior = peso
+        if peso < menor:
+            menor = peso
+print('O maior peso lido é: {}\n O menor peso lido é: {}'.format(maior, menor))
