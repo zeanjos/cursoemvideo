418c13b78f518dfe7980a124599b49585af425ba
zeanjos <pegananjos@gmail.com>
2025-02-20 22:34:05 -0300

    1 hora pra fazer negocio básico desse

---
+++ ex55.py
@@ -0,0 +1,12 @@
+maior = 0
+menor = 0
+for p in range (1,6):
+    peso = float(input('Digite o peso da {}ª Pessoa'))
+    if p == 1:
+        maior == p
+        menor == p 
+else:
+    peso > maior:
+    maior = peso
+    peso < menor:
+    peso = menor
