ffbfa8b8e6c48a51c22131d30e527a313e808c4a
zeanjos <pegananjos@gmail.com>
2025-02-20 22:53:30 -0300

    agora fiz sozinho, antes tinha feito assistindo o vídeo, to começando a entender bem os laços de repetições

--- ex55.py
+++ ex55.py
@@ -1,13 +1,13 @@
 maior = 0
 menor = 0
-for p in range (1,6):
+for p in range(1, 6):
     peso = float(input('Digite o peso da {}ª Pessoa: '.format(p)))
     if p == 1:
         maior = peso
-        menor = peso 
+        menor = peso
     else:
         if peso > maior:
             maior = peso
         if peso < menor:
             menor = peso
-print('O maior peso lido é: {}\n O menor peso lido é: {}'.format(maior, menor))
+print('O maior peso é: {}\n O menor peso é: {}'.format(maior, menor))
