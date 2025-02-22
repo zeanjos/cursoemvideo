664f60c3397dfa35d62d2c991103bb7c674b662e
zeanjos <pegananjos@gmail.com>
2025-02-21 22:22:41 -0300

    alteração que estava causando erro

--- ex56.py
+++ ex56.py
@@ -1,18 +1,23 @@
 maior = 0
 menor = 0
-for n in range(1, 5):
+soma = 0
+for n in range(1, 4):
     print('==={}ª Pessoa==='.format(n))
-    nome = str(input('Nome: '))
+    nome = str(input('Nome: ')).strip().title()
     idade = int(input('Idade: '))
     sexo = str(input('SexO [M/F]: '))
+    soma += idade
     if n == 1:
         maior = idade
         menor = idade
+        name = nome
+        nmenor = nome
     else:
         if idade > maior:
             maior = idade
             name = nome
         if idade < menor:
             menor = idade
-            name = nome
-print('Menor:{} Maior: {}'.format(menor, maior))
+            nmenor = nome
+media = soma /n
+print('Menor idade e nome: {}, {} Maior idade e nome: {}, {}, a média das idades é: {:.2f}'.format(nmenor, menor, maior, name, media))
