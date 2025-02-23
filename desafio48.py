a178d3cd7c388aa5b74b036b236100613bcfed3a
zeanjos <pegananjos@gmail.com>
2025-02-23 19:08:56 -0300

    Modificação

--- desafio48.py
+++ desafio48.py
@@ -3,4 +3,5 @@ for c in range(1, 501):
     if c % 2 == 1:
         if c % 3 == 0:
             soma += c
-print(soma)
+            print('Número impar multiplo de 3: {}'.format(c))
+print('Soma total: {}'.format(soma))
