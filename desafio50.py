e7c3c3faf97b2818893af33b8e932fc09a44a1e4
zeanjos <pegananjos@gmail.com>
2025-02-23 19:31:06 -0300

    Alteração básica que para o úsuario terá uma experiência melhor.

--- desafio50.py
+++ desafio50.py
@@ -1,6 +1,6 @@
 soma = 0
-for c in range(0,6):
-    num = int(input('Digite um número inteiro, para soma de seus numeros pares:'))
+for c in range(1, 7):
+    num = int(input('{} )Digite um número inteiro, para soma de seus numeros pares: '.format(c)))
     if num % 2 == 0:
         soma += num
 print('Os valores que você digitou, somados dão o valor: {}'.format(soma))
