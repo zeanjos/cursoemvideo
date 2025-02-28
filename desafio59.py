2f72f7ac14a48a269785c05d795b6356b10e867f
zeanjos <pegananjos@gmail.com>
2025-02-28 08:27:06 -0300

    O código está sem validação de entrada ou algo do tipo, mas está funcional, ao passar do tempo irei ajustando.

--- desafio59.py
+++ desafio59.py
@@ -2,9 +2,9 @@
 
 menu = None
 maior = 0
-while menu is None:
-    num = float(input('Digite o seu número: '))
-    num2 = float(input('Digite o seu número: '))
+while menu != 5:
+    num = float(input('Digite o primeiro número: '))
+    num2 = float(input('Digite o segundo número: '))
     s = num + num2
     mult = num * num2
     
@@ -16,13 +16,29 @@ while menu is None:
 [4] Novos números\n
 [5] Sair do programa.\n
 Digite a opção desejada: '''))
+    
     if menu == 1:
         print(f'Os números digitados somados dão resultado de: {s:.2f}')
-    if menu == 2:
+    elif menu == 2:
         print(f'Os números digitados multiplicados dão resultado de: {mult:.2f}')
-    if menu == 3:
+    elif menu == 3:
         if num > num2:
             maior = num
         if num2 > num:
             maior = num2
-        print(f'O maior número digitado é: {maior}')
+            print(f'O maior número digitado é: {maior}')
+        if num == num2:
+            print('Os valores são iguais, não existe maior.')
+        desejo = str(input('Deseja continuar? [S/N]: ')).lower()
+        if desejo == 'n':
+            print('Você escolheu sair, tchau!')
+            break
+        elif desejo == 's':
+            print('Continuando...\n')
+    elif menu == 4:
+        print('Você escolheu digitar novos números, boa jornada.')
+    elif menu == 5:
+        print('-=-'*16)
+        print('Opção 5 escolhida, até mais!')
+        print('-=-'*16)
+        
