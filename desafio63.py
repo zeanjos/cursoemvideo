6cc6063d2318576b39add14ebd5272715ecf960c
zeanjos <pegananjos@gmail.com>
2025-03-02 14:00:47 -0300

    Não entendi nada, melhore josé

---
+++ desafio63.py
@@ -0,0 +1,11 @@
+
+num_fim = int(input('Digite o número final: '))
+anterior = 0
+atual = 1
+while num < num_fim:
+    if anterior == 0:
+        anterior = 1
+        att = atual + anterior
+        at_at += att
+    print(at_at)
+        
