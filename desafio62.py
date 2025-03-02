d08b1ae423ac3613858951e35508af881047ae41
zeanjos <pegananjos@gmail.com>
2025-03-02 13:05:36 -0300

    Alteração básica ex 62

--- desafio62.py
+++ desafio62.py
@@ -4,7 +4,10 @@ r = int(input('Digite a razão: '))
 while r <= 10:
     for i in range(p1, 11, r):
         print(f'Resultado: {i}\n')
-    desejo = int(input('Deseja continuar? [0] Não [1] Sim '))
-    if desejo == 0:
+    desejo = int(input('Deseja continuar? [0] Não [1] Sim: '))
+    if desejo == 1:
+        p1 = int(input('Digite o primeiro termo: '))
+        r = int(input('Digite a razão: '))
+    elif desejo == 0:
         print('Você escolheu sair, até mais!')
         break
