9022c271720ffd3dc050515fb641574b771d3999
zeanjos <pegananjos@gmail.com>
2025-03-03 06:58:45 -0300

    Pedi para o copilot fazer para eu tentar entender a lógica, não entendi muito

--- desafio63.py
+++ desafio63.py
@@ -2,10 +2,11 @@
 num_fim = int(input('Digite o número final: '))
 anterior = 0
 atual = 1
-while num < num_fim:
-    if anterior == 0:
-        anterior = 1
-        att = atual + anterior
-        at_at += att
-    print(at_at)
+print(anterior)
+while atual < num_fim:
+    print(f'A sequência de fibonnaci é: {atual}')
+    novo = anterior + atual
+    anterior = atual
+    atual = novo
+    
         
