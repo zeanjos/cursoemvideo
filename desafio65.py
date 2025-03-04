b23b1cce6e0ca8b633006bf136677cb564e932df
zeanjos <pegananjos@gmail.com>
2025-03-04 08:33:16 -0300

    Agora está indo no caminho certo

--- desafio65.py
+++ desafio65.py
@@ -8,25 +8,25 @@ media = 0
 desejo = str('s').lower()
 cont = 0
 while desejo == 's':
-    maior = num
-    menor = num
     num = int(input('Digite o número desejado: '))
     cont += 1
     soma += num
     media = (soma / cont)
-    if num > maior:
+    if maior is None and menor is None:
         maior = num
-        
-    if num > menor:
         menor = num
-        
+    else:
+        if num > maior:
+            maior = num
+        if num < menor:
+            menor = num 
                 
     if desejo == 's':
-        desejo = str(input('Deseja continuar [S] ou [N]: ')).lower()
-        print('Você escolheu continuar, prossiga.') 
+            desejo = str(input('Deseja continuar [S] ou [N]: ')).lower()
+            print('Você escolheu continuar, prossiga.') 
        
-    else:
-        print(f'''A soma dos números digitados é {soma}\n
+    if desejo == 'n':
+        print(f'''A soma dos números digitados é {soma}
 A média dos números digitados é: {media}''')
         print('Você escolheu sair, até mais!')
     
