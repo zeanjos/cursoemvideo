fc582891cf3973264f07c454809910d221781289
zeanjos <pegananjos@gmail.com>
2025-03-07 07:25:27 -0300

    Finalmente deu certo Mundo 2 Desafio 60

--- desafio60f.py
+++ desafio60f.py
@@ -1,9 +1,15 @@
 n = int(input('Digite o número que deseja fatorar: '))
 c = n
-s = 0
+f = 1
+print(f'Calculando !{n} = ', end='')
 while c > 0:
+    print(f'{c}', end='')
+    print(' X ' if c > 1 else ' = ', end='')
+    f *= c
     c -= 1
-    s = n * c
-s *= c
-print(f'{n} x {c} = {s}')
+print(f)
+    #c -= 1
+#s = n * c
+#s *= c
+#print(f'{n} x {c} = {s}')##
 
