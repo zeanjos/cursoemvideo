2d3169caaa2d098f2cf5200f34def071695c4841
zeanjos <pegananjos@gmail.com>
2025-03-08 08:09:29 -0300

    Progressão aritimética feito corretamente!!

--- desafio61.py
+++ desafio61.py
@@ -1,7 +1,18 @@
 p1 = int(input('Digite o primeiro termo: '))
 r = int(input('Digite a razão: '))
-while r <= p1:
-    for i in range(p1, 11, r):
-        print(f'Resultado: {i}')
-    break
+s = p1
+cont = 1
+while cont <=10:
+    for i in range(p1, 11):
+        s += r
+        cont += 1
+        print(f'{s}', end='')
+        if cont <= 10:
+            print(' -> ', end='')
+        else:
+            print('\nFim!')
+        break
+    
+
+
         
