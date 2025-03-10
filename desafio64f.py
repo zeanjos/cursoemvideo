720362865796b67e2c581c4c6751c22852689847
zeanjos <pegananjos@gmail.com>
2025-03-10 08:01:26 -0300

    Sequencia de fibonacci correta, aula 63 Mundo 2

---
+++ desafio64f.py
@@ -0,0 +1,13 @@
+n = int(input('Quantos números você quer mostrar: '))
+t1 = 0
+t2 = 1
+print(f'{t1}, {t2}', end='')
+cont = 3
+t3 = t1 + t2
+while cont < n:
+    t3 = t1 + t2
+    print(f'--> {t3}', end='')
+    t1 = t2
+    t2 = t3
+    cont += 1
+print(' --> FIM')
