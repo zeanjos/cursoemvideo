2f53ec00d7a0028cb8d9588f505d9f1be9370f14
zeanjos <pegananjos@gmail.com>
2025-03-13 06:39:54 -0300

    Desafio 68 mundo 2 Feito ontem mas a internet acabou

--- desafio68.py
+++ desafio68.py
@@ -1,5 +1,25 @@
 from random import randint
-num = randint(1, 2)
+ 
+
 jogador = 0
-while num == jogador:
-    jogador = int(input('Digite um número par ou impar: '))
+cont = 0
+
+while True:
+    num = randint(1, 10)
+    jogador = int(input('Digite um número par ou impar no range de 1 a 10: '))
+    if jogador == 999:
+        print('Você escolheu sair, até mais!')
+        break
+    s = jogador + num
+    parouim = str(input('Par ou impar [P/I]: ')).strip().lower()
+    if s % 2 == 0 and parouim == 'p':
+        print(f'A soma de {jogador} e {num} dão resultado {s} Um número par, você ganhou!')
+    elif s % 2 == 1 and parouim == 'i':
+        cont += 1
+        print(f'A soma de {jogador} e {num} Dão resultado {s} Um número impar, você ganhou')
+    elif s % 2 == 0 and parouim == 'i':
+        cont += 1
+        print(f'A soma de {jogador} e {num} Dão resultado {s} Um número par, você escolheu um número impar, você perdeu!')
+    elif s % 2 == 1 and parouim == 'p':
+        print(f'A soma de {jogador} e {num} Dão resultado {s} Um número impar, você escolheu um número par, você perdeu!')
+    
