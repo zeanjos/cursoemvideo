20ba45237506b58e64778dffd9029fccd10a5eb5
zeanjos <pegananjos@gmail.com>
2025-03-13 07:40:15 -0300

    Modificações boas no código.

--- desafio69.py
+++ desafio69.py
@@ -1,9 +1,21 @@
 idade = contm = contf = maior = menorf = 0
 while True:
-    idade = int(input('Digite sua idade: '))
+    while True:
+        try:
+            idade = int(input('Digite sua idade: '))
+            break
+        except ValueError:
+            print('Valor invalido. Digite um número.')
     if idade > 18:
         maior += 1
-    sexo = str(input('Digite seu sexo: [M/F] ')).strip().lower()
+    while True:    
+        try:
+            sexo = str(input('Digite seu sexo: [M/F] ')).strip().lower()
+            if sexo not in ['m', 'f']:
+                raise ValueError
+            break
+        except ValueError:
+            print('Valor invalido. Digite um sexo correto.')
     if sexo == 'm':
         contm += 1
     else:
@@ -11,11 +23,14 @@ while True:
     if sexo == 'f' and idade < 20:
         menorf += 1
     desejo = str(input('Deseja continuar? [S/N]: '))
+    while desejo != 's' and desejo != 'n':
+        desejo = str(input('Deseja continuar? [S/N]: '))
     if desejo == 'n':
         print('Você escolheu sair!')
         break
-    else:
-        print('Você escolheu continuar!') 
+    if desejo == 's':
+        print('Você escolheu continuar!')
+
 print(f'''{maior} Pessoas tem mais de 18 anos.\n
 foram cadastrados {contm} {'Homem' if contm == 1 else 'homens'}.\n
 {menorf} Mulheres tem menos de 20 Anos.''')
