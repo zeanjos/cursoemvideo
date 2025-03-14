ab38bf076fb4b0ef5ef5cc44bd4a575ccc078950
zeanjos <pegananjos@gmail.com>
2025-03-14 06:55:23 -0300

    Modificações que melhoram o codigo

--- desafio68.py
+++ desafio68.py
@@ -13,13 +13,13 @@ while True:
     s = jogador + num
     parouim = str(input('Par ou impar [P/I]: ')).strip().lower()
     if s % 2 == 0 and parouim == 'p':
-        print(f'A soma de {jogador} e {num} dão resultado {s} Um número par, você ganhou!')
+        print(f'A soma de {jogador} e {num} dão resultado {s} Um número par, você ganhou! Sua sequência foi de {'Vitória' if cont == 1 else 'Vitórias'}')
     elif s % 2 == 1 and parouim == 'i':
         cont += 1
-        print(f'A soma de {jogador} e {num} Dão resultado {s} Um número impar, você ganhou')
+        print(f'A soma de {jogador} e {num} Dão resultado {s} Um número impar, você ganhou! Sua sequência foi de {'Vitória' if cont == 1 else 'Vitórias'}')
     elif s % 2 == 0 and parouim == 'i':
         cont += 1
-        print(f'A soma de {jogador} e {num} Dão resultado {s} Um número par, você escolheu um número impar, você perdeu!')
+        print(f'A soma de {jogador} e {num} Dão resultado {s} Um número par, você escolheu um número impar, você perdeu! Sua sequência foi de {'Vitória' if cont == 1 else 'Vitórias'}.')
     elif s % 2 == 1 and parouim == 'p':
-        print(f'A soma de {jogador} e {num} Dão resultado {s} Um número impar, você escolheu um número par, você perdeu!')
+        print(f'A soma de {jogador} e {num} Dão resultado {s} Um número impar, você escolheu um número par, você perdeu! Sua sequência foi de {'Vitória' if cont == 1 else 'Vitórias'}')
     
