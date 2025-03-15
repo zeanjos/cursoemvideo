261ed6e87531b432c7fe3882d2317065fe88edc7
zeanjos <pegananjos@gmail.com>
2025-03-15 07:43:14 -0300

    Desafio 71 Mundo 2 Parece que não mas passei 1 hora escrevendo isso aqui, lógica purinha.

--- desafio71.py
+++ desafio71.py
@@ -1,1 +1,25 @@
 valor = int(input('Qual valor você irá sacar? '))
+cem = cinq = vin = dez = cin = um = 0
+
+while True:
+        if valor >= 100:
+            valor -= 100
+            cem += 1
+        elif valor >= 50:
+            valor -= 50
+            cinq += 1
+        elif valor >= 20:
+            valor -= 20
+            vin += 1
+        elif valor <= 19:
+            valor -= 10
+            dez += 1
+        elif valor < 10:
+            valor -= 5
+            cin += 1
+        elif valor < 5:
+            valor -= 1
+            um += 1
+        if valor == 0:
+            break
+print(f'Foram entregues {cem} notas de cem, {cinq} notas de cinquenta, {vin} notas de vinte, {dez} notas de dez, {cin} notas cinco e {um} moedas de um real')
