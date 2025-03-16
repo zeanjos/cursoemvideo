ba3b39d6dcd88f6c590aeeebb5f857bd12a27b87
zeanjos <pegananjos@gmail.com>
2025-03-16 13:50:22 -0300

    Agora o código tá certo

--- desafio68.py
+++ desafio68.py
@@ -6,20 +6,27 @@ cont = 0
 
 while True:
     num = randint(1, 10)
-    jogador = int(input('Digite um número par ou impar no range de 1 a 10: '))
+    print('==' * 20)
+    jogador = int(input('Digite um número par ou impar no range de 1 a 10, se desejar sair digite 999:  '))
+    print('==' * 20)
     if jogador == 999:
         print('Você escolheu sair, até mais!')
         break
+
     soma = jogador + num
     parouim = str(input('Par ou impar [P/I]: ')).strip().lower()[0]
+
     if soma % 2 == 0 and parouim == 'p':
-        print(f'A soma de {jogador} e {num} dão resultado {soma} Um número par, você ganhou! Sua sequência foi de {'Vitória' if cont == 1 else 'Vitórias'}')
+        cont += 1
+        print(f'A soma de {jogador} e {num} dão resultado {soma} Um número par, você ganhou! Sua sequência foi de {cont} {'Vitória' if cont == 1 else 'Vitórias'}')
+
     elif soma % 2 == 1 and parouim == 'i':
         cont += 1
-        print(f'A soma de {jogador} e {num} Dão resultado {soma} Um número impar, você ganhou! Sua sequência foi de {'Vitória' if cont == 1 else 'Vitórias'    }')
+        print(f'A soma de {jogador} e {num} Dão resultado {soma} Um número impar, você ganhou! Sua sequência foi de {cont} {'Vitória' if cont == 1 else 'Vitórias'    }')
+
     elif soma % 2 == 0 and parouim == 'i':
-        cont += 1
-        print(f'A soma de {jogador} e {num} Dão resultado {soma} Um número par, você escolheu um número impar, você perdeu! Sua sequência foi de {'Vitória' if cont <= 1 else 'Vitórias'}.')
+        print(f'A soma de {jogador} e {num} Dão resultado {soma} Um número par, você escolheu um número impar, você perdeu! Sua sequência foi de {cont} {'Vitória' if cont <= 1 else 'Vitórias'}.')
+        break
     elif soma % 2 == 1 and parouim == 'p':
-        print(f'A soma de {jogador} e {num} Dão resultado {soma} Um número impar, você escolheu um número par, você perdeu! Sua sequência foi de {'Vitória' if cont <= 1 else 'Vitórias'}')
-    
+        print(f'A soma de {jogador} e {num} Dão resultado {soma} Um número impar, você escolheu um número par, você perdeu! Sua sequência foi de {cont} {'Vitória' if cont <= 1 else 'Vitórias'}')
+        break
