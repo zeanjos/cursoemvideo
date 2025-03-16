f5ec82d50fc79d5c4398a668fcf3409574311253
zeanjos <pegananjos@gmail.com>
2025-03-16 13:35:32 -0300

    Boas práticas no código.

--- desafio68.py
+++ desafio68.py
@@ -10,16 +10,16 @@ while True:
     if jogador == 999:
         print('Você escolheu sair, até mais!')
         break
-    s = jogador + num
+    soma = jogador + num
     parouim = str(input('Par ou impar [P/I]: ')).strip().lower()[0]
-    if s % 2 == 0 and parouim == 'p':
-        print(f'A soma de {jogador} e {num} dão resultado {s} Um número par, você ganhou! Sua sequência foi de {'Vitória' if cont == 1 else 'Vitórias'}')
-    elif s % 2 == 1 and parouim == 'i':
+    if soma % 2 == 0 and parouim == 'p':
+        print(f'A soma de {jogador} e {num} dão resultado {soma} Um número par, você ganhou! Sua sequência foi de {'Vitória' if cont == 1 else 'Vitórias'}')
+    elif soma % 2 == 1 and parouim == 'i':
         cont += 1
-        print(f'A soma de {jogador} e {num} Dão resultado {s} Um número impar, você ganhou! Sua sequência foi de {'Vitória' if cont == 1 else 'Vitórias'    }')
-    elif s % 2 == 0 and parouim == 'i':
+        print(f'A soma de {jogador} e {num} Dão resultado {soma} Um número impar, você ganhou! Sua sequência foi de {'Vitória' if cont == 1 else 'Vitórias'    }')
+    elif soma % 2 == 0 and parouim == 'i':
         cont += 1
-        print(f'A soma de {jogador} e {num} Dão resultado {s} Um número par, você escolheu um número impar, você perdeu! Sua sequência foi de {'Vitória' if cont <= 1 else 'Vitórias'}.')
-    elif s % 2 == 1 and parouim == 'p':
-        print(f'A soma de {jogador} e {num} Dão resultado {s} Um número impar, você escolheu um número par, você perdeu! Sua sequência foi de {'Vitória' if cont <= 1 else 'Vitórias'}')
+        print(f'A soma de {jogador} e {num} Dão resultado {soma} Um número par, você escolheu um número impar, você perdeu! Sua sequência foi de {'Vitória' if cont <= 1 else 'Vitórias'}.')
+    elif soma % 2 == 1 and parouim == 'p':
+        print(f'A soma de {jogador} e {num} Dão resultado {soma} Um número impar, você escolheu um número par, você perdeu! Sua sequência foi de {'Vitória' if cont <= 1 else 'Vitórias'}')
     
