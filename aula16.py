d9471ed22b0b1c81193b4c52df1c224a06c51603
zeanjos <pegananjos@gmail.com>
2025-03-19 08:33:36 -0300

    Aula 16 Mundo 3 !!!! Tuplas

---
+++ aula16.py
@@ -0,0 +1,4 @@
+lanche = ('Hamburguer', 'Suco', 'Pizza', 'Pudim')
+# Tuplas são imutaveis!
+# Este comando está errado pois tuplas são imutaveis, aqui está tentando fazer a atribuição de outra string lanche[1] = 'Refrigerante'
+print(lanche[1])
