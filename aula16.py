84198db4c088fd1947772d20202f36ca42a3d92d
zeanjos <pegananjos@gmail.com>
2025-03-19 08:46:29 -0300

    Aula muito boa, depois do trampo eu estudo mais!

--- aula16.py
+++ aula16.py
@@ -1,4 +1,13 @@
 lanche = ('Hamburguer', 'Suco', 'Pizza', 'Pudim')
+
+#for cont in range(0, len(lanche)):
+#    print(f'Eu vou comer {lanche[cont]} na posição {cont}')
+
+
+
+
 # Tuplas são imutaveis!
 # Este comando está errado pois tuplas são imutaveis, aqui está tentando fazer a atribuição de outra string lanche[1] = 'Refrigerante'
-print(lanche[1])
+for pos, comida in enumerate(lanche):
+   print(f'Eu vou comer {comida} na posição {pos}')
+#print('É o bulking não tem jeito')
