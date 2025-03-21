828cad4208653ae1ea7b9c40b061a29c9ee5574d
zeanjos <pegananjos@gmail.com>
2025-03-20 21:33:59 -0300

    Desafio 73 Concluido!

--- desafio73.py
+++ desafio73.py
@@ -1,14 +1,18 @@
 jogadores = ('Botafogo', 'Palmeiras', 'Flamengo', 'Fortaleza', 'Internacional',
              'São Paulo', 'Corinthians', 'Bahia', 'Cruzeiro', 'Vasco da Gama', 
              'EC Vitória', 'Atlético-MG', 'Fluminense', 'Grêmio', 'Juventude', 
-             'Bragantino', 'Athletico-PR', 'Criciúma', 'Atlético-GO', 'Cuiabá'
+             'Bragantino', 'Athletico-PR', 'Chapecoense', 'Atlético-GO', 'Cuiabá'
 )
 ultimos4 = jogadores[-4:]
 nome = sorted(jogadores)
 
-for pos, valor in enumerate(jogadores):
 
-    print('Os cinco primeiros colocados são: ', jogadores[0: 5])
-    print(f'Os últimos quatro colocados da tabela são: {ultimos4}')
-    print(f'O nome dos Times em ordem alfabética: {nome}')
-    print(pos)
+
+print('Os cinco primeiros colocados são: ', jogadores[0: 5])
+print('='*40)
+print(f'Os últimos quatro colocados da tabela são: {ultimos4}')
+print('='*40)
+print(f'O nome dos Times em ordem alfabética: {nome}')
+print('='*40)
+print(f'Chapecoense está em {jogadores.index('Chapecoense')+ 1}º Na tabela de colocados.')
+print('='*40)
