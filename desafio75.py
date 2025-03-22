2689ca9fedc032d637b0aafd7478638d033a54cc
zeanjos <pegananjos@gmail.com>
2025-03-21 21:37:00 -0300

    Ainda não consegui fazer Desafio 75, vou passar de desafio pois já estou travado a muitas horas nisso.

--- desafio75.py
+++ desafio75.py
@@ -1,20 +1,27 @@
 cont = 0
 cont_nove = 0
 valores = str()
-
+primeiro_tres = 0
+qntd = 0
 while cont < 4:
-    try:
-
         num = int(input('Digite um número: '))
         num_str = str(num)
         cont += 1
         valores += num_str + ', '
         cont_nove += num_str.count('9')
-        if '3' in valores:
-            cont_tres = valores.index('3')
-    except ValueError:
-        print('Digito invalido, digite novamente.')
+
+        try:
+            num_f = int(num_str.index('3'))
+        except ValueError:
+              print('Não possui numero 3')
+        else:
+            print(num_f)
+            if primeiro_tres == 0:
+                  primeiro_tres = num_f
+                  ind_pri_tres = cont
+        
+            
         
 
 
-print(f'Valores: {valores} Quantidade de vezes que o 9 apareceu: ', cont_nove, f'O primeiro número três apareceu em indice: {cont_tres}')
+print(f'Valores: {valores} \nQuantidade de vezes que o 9 apareceu: ', cont_nove, f'\nO primeiro número três apareceu em indice:{ind_pri_tres}')
