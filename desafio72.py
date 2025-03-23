293420f21e988dd754ed1e1b99a113fb6b4a8d5b
zeanjos <pegananjos@gmail.com>
2025-03-22 22:13:56 -0300

    Assistindo a aula do exercicio fiz um cód melhor e entendi mais também

--- desafio72.py
+++ desafio72.py
@@ -1,7 +1,17 @@
-lista1 = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
-lista2 = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezeseis', 'dezessete', 'dezoito', 'dezenove', 'vinte' )
-num = int(input('Digite um número entre 0 e 20: '))
-if num in lista1:
-    print(f'Voce digitou o número "{num}" Por extenso fica', lista2[num])
-else:
-    print('Erro, você digitou incorretamente')
+lista1 = (0, 1, 2, 3, 4,
+           5, 6, 7, 8,
+             9, 10, 11, 12,
+               13, 14, 15, 16,
+                    17, 18, 19, 20)
+lista2 = ('zero', 'um', 'dois', 'três', 'quatro',
+           'cinco', 'seis', 'sete', 'oito',
+             'nove', 'dez', 'onze', 'doze',
+               'treze', 'quatorze', 'quinze', 'dezeseis',
+                 'dezessete', 'dezoito', 'dezenove', 'vinte' )
+
+while True:
+    num = int(input('Digite um número entre 0 e 20: '))
+    if 0 <= num <= 20:
+        break
+    print('Você digitou errado.')
+print(f'Você digitou o número{lista1[num]} E por extenso fica "{lista2[num]}"')
--- desafio77.py
+++ desafio77.py
@@ -1,3 +1,3 @@
 palavras = ("batata", "abobora", "berinjela", "repolho")
-if 'a' or "e" or "i" or "u" in palavras[0]:
-    print(palavras[0])
+palavras[0].count('a')
+    
