6fc818568c62c8bc83f684e2adf5693cc4ba30e9
zeanjos <pegananjos@gmail.com>
2025-03-28 08:38:50 -0300

    To tentando to tentando

--- desafio78.py
+++ desafio78.py
@@ -1,18 +1,14 @@
 from math import inf
-menor = inf
-maior = 0
 num = []
-lista_menor = indice_menor = []
-for contagem in range(0, 5):
-    num.append(int(input('Digite um número: ')))
-for c, v in enumerate(num):
-    if menor > num[c]:
-        menor = num[c]
-        indice = c
-        if menor == num[c]:
-            lista_menor.append(v)
-            indice_menor.append(c)
-    if maior < num[c]:
-        maior = num[c]
-        indice_maior = c
-print(f'O menor número digitado foi: {menor} no indice {indice}.\nO maior número digitado foi: {maior} no indice: {indice_maior}, indice do menor 2x: {indice_menor}, lista do menor 2x: {lista_menor}')
+menor = inf
+while True:
+    
+        num.append(int(input('Digite um número: ')))
+        for i, v in enumerate(num): # I de indice, V de valor
+            if num[-1] == v:
+                num.pop[-1]
+                if menor > v:
+                  menor = v
+                print('Você digitou um número que já existe.')
+            print(f'Indice {i} valor {v}, menor é {menor}')
+        
