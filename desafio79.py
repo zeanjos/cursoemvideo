3a7ef9d1b596dde7ec0cb6abf7c0de691f110618
zeanjos <pegananjos@gmail.com>
2025-03-29 08:48:22 -0300

    Está dando andamento

--- desafio79.py
+++ desafio79.py
@@ -1,29 +1,22 @@
-from math import inf
-num = []
-menor = inf
-indice = -1
-while True:
-    novo_num = int(input('Digite um número: '))
-    
-   
-    if novo_num  in num:
-        print(f'Valor {novo_num}, já está na lista')
-    else:
-        num.append(novo_num)
-    if len(num) > 0:
-        menor = inf
-        for c, v in enumerate(num): # C é o indice e o V  valor
-            if menor > v:
-                menor = v
-                indice = c
 
-    desejo = str(input('Deseja continuar [S/N] ? ')).lower().strip()
-    if desejo == 's':
-        print('Você escolheu continuar!')
-    elif desejo == 'n':
-        print('Você escolheu parar!')
-        break
-    else:
-        print('Você digitou um caracter incorreto!')
-        break
-print(f'Menor número: {menor}, no indice {indice}')
+numeros = []
+indice = 0
+continuar = True
+while continuar:
+    for i in range(0, indice + 1):
+        print('-'*30)
+        num = int(input('Digite um número: '))
+        print('-'*30)
+        numeros.append(num)
+
+        desejo = str(input('Deseja continuar [S/N] ?')).lower().strip()
+        if desejo == 's':
+            print('Você escolheu continuar!')
+        elif desejo == 'n':
+            print('Você escolheu sair!')
+            continuar = False
+      
+            
+        
+        
+    
