f5607586a61e54d4b2cbe9547e4e5b0a547938f4
zeanjos <pegananjos@gmail.com>
2025-03-29 20:45:52 -0300

    Consegui!

--- desafio79.py
+++ desafio79.py
@@ -1,22 +1,19 @@
-
 numeros = []
-indice = 0
 continuar = True
+num_organizado = None
 while continuar:
-    for i in range(0, indice + 1):
-        print('-'*30)
-        num = int(input('Digite um número: '))
-        print('-'*30)
+    num = int(input('Digite um número: '))
+    if num in numeros:
+        print('Este número já existe e não será adicionado')
+    else:
         numeros.append(num)
-
-        desejo = str(input('Deseja continuar [S/N] ?')).lower().strip()
-        if desejo == 's':
-            print('Você escolheu continuar!')
-        elif desejo == 'n':
-            print('Você escolheu sair!')
-            continuar = False
-      
-            
-        
-        
-    
+    desejo = str(input('Deseja continuar [S/N]? ')).lower().strip()
+    if desejo == 's':
+        print('Você escolheu continuar!')
+    if desejo == 'n':
+        numeros.sort()
+        print(f'Você escolheu parar, a lista em ordem com os números digitados: {numeros}')
+        continuar = False
+    if desejo not in ['s', 'n']:
+        print('Você digitou um caracter incorreto!')
+        continuar = False
