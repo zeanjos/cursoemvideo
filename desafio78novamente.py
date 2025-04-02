ab71a00550ece27ac570f39898140c3423b95c55
zeanjos <pegananjos@gmail.com>
2025-04-02 08:39:15 -0300

    Feito com outra lógica seguindo tutorial

--- desafio78novamente.py
+++ desafio78novamente.py
@@ -1,10 +1,25 @@
+listanum = []
+maior = menor = 0
+for c in range (0, 5):
+    listanum.append(int(input(f'Digite um valor para a posição "{c}": ')))
+    if c == 0:
+        menor = maior = listanum[c]
+    else:
+        if listanum[c] > maior:
+            maior = listanum[c]
+        if listanum[c] < menor:
+            menor = listanum[c]
+print('-'*30)
+print(f"A lista: {listanum}")
+print(f'O maior valor digitado foi {maior} nas posições ', end='')
 
+for i, v in enumerate(listanum):
+    if v == maior:
+        print(f'{i}...', end='')
+print()
+print(f'O menor valor digitado foi {menor} nas posições ', end="")
+for i, v in enumerate(listanum):
+    if v == menor:
+        print(f'{i}...', end='')
 
-num = []
-indice_menor = []
-
-for contagem in range(0, 5):
-    num.append(int(input('Digite um número: ')))
-for c, v in enumerate(num):
-    if menor_num > num[c]:
-        menor_num = num[c]
+    
