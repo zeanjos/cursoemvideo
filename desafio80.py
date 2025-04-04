cf4c2e0e1d9bd2f0b25fafd1fa6d6abd5c85e3c7
zeanjos <pegananjos@gmail.com>
2025-04-04 08:30:43 -0300

    Alteração

--- desafio80.py
+++ desafio80.py
@@ -3,12 +3,14 @@ for c in range(0, 5):
     print('-'*30)
     num = int(input('Digite um valor: '))
     if c == 0 or num > numeros[-1]:
+        print('Adicionado ao final da lista.')
         numeros.append(num)
     else:
         pos = 0
         while pos < len(numeros):
             if num <= numeros[pos]:
                 numeros.insert(pos, num)
+                print(f'Adicionado na posição {pos}')
                 break
             pos += 1
 print('-'*30)
