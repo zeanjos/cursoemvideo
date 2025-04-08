dbea8a8935256775bb5a4118981551116f542259
zeanjos <pegananjos@gmail.com>
2025-04-08 20:54:31 -0300

    Confesso que não me esforcei 100% to cansado e só consegui resolver fazendo o copilot de professor.

--- desafio84.py
+++ desafio84.py
@@ -1,11 +1,8 @@
 lista = []
 dados = []
-maior_peso = 0
-menor_peso = None
 continua = True
-menor_peso_nome = []
+menor_peso_nome = maior_peso_nome = []
 while continua:
-    for c in range(0, continua):
         dados.append(str(input('Digite o nome: ')))
         dados.append(int(input('Digite o peso: ')))
         lista.append(dados[:])
@@ -19,13 +16,14 @@ while continua:
         else:
             print('Voce digitou um caracter invalido.')
 for p in lista:
-    if menor_peso is None:
-        menor_peso = p[1]
-    if p[1] > maior_peso:
-                maior_peso = p[1]
-    if p[1] < menor_peso:
-        menor_peso = p[0]
-        menor_peso_nome = p[0], p[1]
+    if not menor_peso_nome:
+        menor_peso_nome = [p[0], p[1]]
+    if not maior_peso_nome:
+        maior_peso_nome = [p[0], p[1]]
+    if p[1] > maior_peso_nome[1]:
+        maior_peso_nome = [p[0], p[1]]
+    if p[1] < menor_peso_nome[1]:
+        menor_peso_nome = [p[0], p[1]]
 tamanho_lista = len(lista)
 print('-'*30)
-print(f'Você cadastrou {tamanho_lista} Pessoas Maior peso {maior_peso}\n Menor peso {menor_peso_nome}')
+print(f'Você cadastrou {tamanho_lista}\n Pessoas Maior peso {maior_peso_nome}\n Menor peso {menor_peso_nome}')
