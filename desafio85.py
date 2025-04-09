93d1f7bf408b47fc1ee212fe13739c80cd39534f
zeanjos <pegananjos@gmail.com>
2025-04-09 20:52:45 -0300

    Só consegui com o copilot me explicando, mas boora

--- desafio85.py
+++ desafio85.py
@@ -1,15 +1,15 @@
 lista_numeros = []
 numeros = []
-pares = impares = []
+pares = []
+impares = []
 for c in range(1, 8):
     numeros.append(int(input(f'Digite o {c} Valor: ')))
     lista_numeros.append(numeros[:])
     numeros.clear()
-for sub_lista in lista_numeros:
-        
+for sub_lista in lista_numeros: 
     if sub_lista[0] % 2 == 0:
-        pares.append(sub_lista)
+        pares.append(sub_lista[0])
     else:
-        impares.append(sub_lista)
-lista_numeros.append([pares, impares])
-print(lista_numeros)
+        impares.append(sub_lista[0])
+lista_numeros = [pares, impares]
+print(f'Números pares {lista_numeros[0]}\n' if len(pares)  > 0 else 'Não foi digitado numeros pares' + '\n', f'Lista impares {lista_numeros[1]}' if len(impares) > 0 else 'Não foi digitado numeros impares.')
