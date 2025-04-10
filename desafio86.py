05eeeb0980b1db1ae6dd9cf468c4cb0253ff114b
zeanjos <pegananjos@gmail.com>
2025-04-10 20:46:40 -0300

    Quase acabando, to sem saco pra fazer isso hj

--- desafio86.py
+++ desafio86.py
@@ -4,5 +4,7 @@ matriz = [
     [0,0,0]
 ]
 for linha in range(0, 3):
-    num = int(input('Digite um numero'))
-    matriz.append([0])
+    for coluna in range(0, 3):
+        num = int(input(f'Digite um numero para a posição {linha}, {coluna} : '))
+        matriz[linha] [coluna] = num
+print(matriz[linha])
