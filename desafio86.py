9363035876ccaf3c6fb22994bb37f39f5f8b96a0
zeanjos <pegananjos@gmail.com>
2025-04-11 07:48:37 -0300

    Feito!

--- desafio86.py
+++ desafio86.py
@@ -7,4 +7,8 @@ for linha in range(0, 3):
     for coluna in range(0, 3):
         num = int(input(f'Digite um numero para a posição {linha}, {coluna} : '))
         matriz[linha] [coluna] = num
-print(matriz[linha])
+print('-'*30,'\nMatriz:\n','-'*30)
+for linha in matriz:
+    print(linha)
+print('-'*30)
+
