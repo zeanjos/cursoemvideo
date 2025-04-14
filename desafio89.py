1f434e35cca937ecf75b5609890a38a63803f982
zeanjos <pegananjos@gmail.com>
2025-04-14 07:51:25 -0300

    Tentei alinhar mas não me lembro como faz

--- desafio89.py
+++ desafio89.py
@@ -7,17 +7,23 @@ while continua:
     nota = float(input(f'Digite a primeira nota do aluno {nome}: '))
     segunda_nota = float(input(f'Digite a segunda nota do aluno {nome}: '))
     media = (nota + segunda_nota) / 2
-    aluno = [nome, nota, segunda_nota, media]
+    aluno = [nome, media]
     dados.append(aluno)
 
-    desejo = str(input('Deseja cadastrar outra nota de aluno [S/N]? ')).lower()
-    if desejo not in ['s', 'n']:
-        print('Entrada inválida. Por favor, insira "S" para continuar ou "N" para sair.')
-        break
-    if desejo == 's':
-        print('Sua escolha foi continuar!')
-    elif desejo == 'n':
-        break
-print(dados)
-    
+    while continua:
+        desejo = str(input('Deseja cadastrar outra nota de aluno [S/N]? ')).lower()
+        if desejo not in ['s', 'n']:
+            print('Entrada inválida. Por favor, insira "S" para continuar ou "N" para sair.')
+        if desejo == 's':
+            print('Sua escolha foi continuar!')
+            break
+        elif desejo == 'n':
+            print('Voce escolheu parar!')
+            continua = False
+print('-'*30)
+print('Nº  Nome        Média')
+dados_formatados = [[aluno[0], f'{aluno[1]:.2f}']for aluno in dados]
+for i, enumerados in enumerate(dados_formatados):
+    print(f'{i} - {" |      ".join(enumerados)}')
+
     
