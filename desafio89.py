d6780a9db8fbcff5fa34749f5f45941a2fa38963
zeanjos <pegananjos@gmail.com>
2025-04-14 21:39:54 -0300

    Mais de dois dias pra organizar e alinhar textos meu deus do ceu

--- desafio89.py
+++ desafio89.py
@@ -10,20 +10,19 @@ while continua:
     aluno = [nome, media]
     dados.append(aluno)
 
-    while continua:
-        desejo = str(input('Deseja cadastrar outra nota de aluno [S/N]? ')).lower()
-        if desejo not in ['s', 'n']:
-            print('Entrada inválida. Por favor, insira "S" para continuar ou "N" para sair.')
-        if desejo == 's':
-            print('Sua escolha foi continuar!')
-            break
-        elif desejo == 'n':
-            print('Voce escolheu parar!')
-            continua = False
+    desejo = str(input('Deseja cadastrar outra nota de aluno [S/N]? ')).lower()
+    if desejo not in ['s', 'n']:
+        print('Entrada inválida. Por favor, insira "S" para continuar ou "N" para sair.')
+    if desejo == 's':
+        print('Sua escolha foi continuar!')
+        continua
+    elif desejo == 'n':
+        print('Voce escolheu parar!')
+        continua = False
 print('-'*30)
-print(f'Nº - Nome {'media':>15}')
+print(f'{'Nº':<4} - {'Nome':<10} {'|':^3}{'Media':>8}')
 dados_formatados = [[aluno[0], f'{aluno[1]:.2f}']for aluno in dados]
 for i, enumerados in enumerate(dados_formatados):
-    print(f'{i:02} - {(f"{item:<15}" for item in enumerados)}')
+    print(f'{i:<4} - {enumerados[0]:<10} {'|':^3} {enumerados[1]:>6}')
 
     
