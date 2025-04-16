59e65c1a6195f1ac5cfe82970b0fab4255adecbc
zeanjos <pegananjos@gmail.com>
2025-04-15 21:42:37 -0300

    Acho que tá quase finalizado, só falta o join, fiz mais rapido dessa vez

--- desafio84tutorial.py
+++ desafio84tutorial.py
@@ -1,14 +1,33 @@
+lista = []
 dados = []
-peso = []
-menor_peso = []
-
+menor = []
+maior = []
 continua = True
 contador = 0
 while continua:
     contador += 1
-    peso.append(str(input(f'Digite o nome da {contador}ª Pessoa: ')))
-    peso.append(float(input(f'Digite o peso da {contador}ª Pessoa: ')))
+    
+    dados.append(str(input(f'Digite o nome da {contador}ª Pessoa: ')).capitalize().strip())
+    dados.append(float(input(f'Digite o peso da {contador}ª Pessoa: ')))
+    lista.append(dados[:])
+    
+    if contador == 1:
+        menor = dados[1]
+        maior = dados[1]
+    if dados [1] < menor:
+        menor = dados [1]
+    if dados [1] > maior:
+        maior = dados [1]
+    dados.clear()
 
     desejo = str(input('Deseja continuar [S/N]? '))
     if desejo == 's':
         print('Você escolheu continuar.')
+    if desejo == 'n':
+        print('-'*30)
+        print('Você escolheu parar')
+        continua = False
+
+print('-'*30)
+print(f'\nOs dados foram: {lista} e menor também: {menor}\nAo todo você cadastrou {len(lista)} Pessoas.\n')
+print('-'*30)
