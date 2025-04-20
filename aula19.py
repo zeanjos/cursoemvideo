a320fc9eac305425c205c9c2580db38e868c58eb
zeanjos <pegananjos@gmail.com>
2025-04-20 08:43:34 -0300

    Testes aula 19

--- aula19.py
+++ aula19.py
@@ -3,9 +3,18 @@ filme = {'titulo': 'Starwars',
          'Idade minima permitida': '14 Anos',
          'ano': '1976'
          }
+filme_um = {'titulo': 'Robocop',
+            'Genero': 'Ação',
+            'Idade minima permitida:': '16 Anos',
+            'Ano': '2014'
+            
+            }
 ##for chave in filme:
     ##print(f'{chave}: {filme[chave]}')
-
+lista_filmes = [filme, filme_um]
+for l in lista_filmes:
+    print(f'{l}:')
+print()
 
 print(filme.values())#Aqui irá mostrar apenas os valores dentro do dicionario, por exemplo starwars, 
 #é um valor que está dentro do dicionario
