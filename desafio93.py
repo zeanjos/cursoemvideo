a86cd88bbd090c1e2047e5d4f942641a06e40798
zeanjos <pegananjos@gmail.com>
2025-04-25 21:51:38 -0300

    Só consegui pq a IA me ajudou

--- desafio93.py
+++ desafio93.py
@@ -1,9 +1,13 @@
 aproveitamento = {}
-
+total = 0
 aproveitamento['nome'] = str(input('Digite o nome do jogador: ')).strip().capitalize()
 aproveitamento['partidas'] = int(input(f'Digite quantas partidas "{aproveitamento['nome']}" jogou: ').strip())
 for c in range (1, aproveitamento['partidas'] + 1):
-    aproveitamento['gols'] = int(input(f'Digite a quantidade de gols feito na {c}ª partida: ').strip())
-    aproveitamento[f'jogo{c}']
-for k, v in range (aproveitamento.items):
-    print(k, v)
+    aproveitamento[f'jogo{c}'] = int(input(f'Digite a quantidade de gols feito na {c}ª Partida: '))
+print(f'O jogador {aproveitamento['nome']}, jogou {aproveitamento['partidas']} Partidas')
+for c, v in aproveitamento.items():
+    if 'jogo' in c:
+        print(f'Jogo {c[4:]}: {v}')
+        total += v
+
+print(f'Foi um total de {total}')
